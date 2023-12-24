5d8ced7deef48b4dbba66e3a581d85b7091d9483
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-24 23:45:55 +0530

    commit

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%=ebhrtfz$*9^tc*=xza851ghsj+0mtpq+o&)_ddt30jygo6iv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
