21277a661a7fcfe779995ead43502e4efb1176fa
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-25 10:50:15 +0530

    commit

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%=ebhrtfz$*9^tc*=xza851ghsj+0mtpq+o&)_ddt30jygo6iv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','*']
 
@@ -123,6 +123,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
