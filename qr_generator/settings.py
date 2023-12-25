953b120c21c1b9e34ae3bf8846628b098b7c48f8
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-25 11:30:18 +0530

    commit

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = "qr_generator.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
